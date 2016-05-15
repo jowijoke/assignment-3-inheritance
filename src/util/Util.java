1c4ce9e35967acb7e3e97108ceae2cb7689e44f1
John Kennedy <johnwjken@gmail.com>
2016-05-15 20:07:01 +0100

    Final draft

--- src/util/Util.java
+++ src/util/Util.java
@@ -1,34 +1,36 @@
-package util; 
+package util;
 
 import ie.wit.ictskills.shapes.Measurable;
 
 import java.util.ArrayList;
 
-public class Util 
+public class Util
 {
-	/**
-	 * Measureable interface contains the method double perimeter().
-	 * The method maximum here evalutates the single value representing the largest
-	 * perimeter discovered in the list of Measurable objects.
-	 *
-	 * @param object The list of objects whose classes implement the interface Measurable
-	 * @return Returns the largest perimeter discovered among entire list objects.
-	 */
-	static public double maximum(ArrayList<Measurable> measurables) 
-	{
-		double max = 0;
-     
-     for(Measurable shape : measurables)
-     {
-       shape.perimeter();
-               
-     if(shape.perimeter() > max)
-     {
-       max = shape.perimeter();
-       
-     }
-    
-     } 
-     return max;
-	}
+  /**
+   * Measureable interface contains the method double perimeter(). The method
+   * maximum here evalutates the single value representing the largest perimeter
+   * discovered in the list of Measurable objects.
+   *
+   * @param object
+   *          The list of objects whose classes implement the interface
+   *          Measurable
+   * @return Returns the largest perimeter discovered among entire list objects.
+   */
+  static public double maximum(ArrayList<Measurable> measurables)
+  {
+    double max = 0;
+
+    for (Measurable shape : measurables)
+    {
+      shape.perimeter();
+
+      if (shape.perimeter() > max)
+      {
+        max = shape.perimeter();
+
+      }
+
+    }
+    return max;
+  }
 }
